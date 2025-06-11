e420749c2119d4e716325dd92419a8acca375a5b
reza-rayan <reza.hosseinzadeh@elanza.com>
2025-06-11 10:17:13 +0330

    Add env configurations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "log"
+import (
+	"github.com/Reza-Rayan/social_golang/internal/env"
+	"log"
+)
 
 func main() {
 	cfg := config{
-		address: ":5000",
+		address: env.GetString("ADDRESS", ":5000"),
 	}
 	app := &application{
 		config: cfg,
